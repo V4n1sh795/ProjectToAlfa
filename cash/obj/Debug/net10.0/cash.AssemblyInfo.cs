55d164f2f14869cf565ee70d6016a3eaddff2c16
V4n1sh795 <v4n1shnerush@gmail.com>
2026-04-14 14:35:24 +0500

    prod frontebd build

--- cash/obj/Debug/net10.0/cash.AssemblyInfo.cs
+++ cash/obj/Debug/net10.0/cash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9053820b0fcdb55d47a5db0cff2b7df5452cae65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7375e1448c8c970847e42ca021f4a8aa97994df6")]
 [assembly: System.Reflection.AssemblyProductAttribute("cash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
