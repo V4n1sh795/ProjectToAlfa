aaac1b080c76cb40ab0fc9448d653c9158e93c14
V4n1sh795 <v4n1shnerush@gmail.com>
2026-06-03 15:56:03 +0500

    branch init

--- cash/obj/Debug/net10.0/cash.AssemblyInfo.cs
+++ cash/obj/Debug/net10.0/cash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a977a7615ed476698ac6dbc32604c4e5c53f1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e21e8e5dbfff520f26774383bf9f67b04f8b0f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("cash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
