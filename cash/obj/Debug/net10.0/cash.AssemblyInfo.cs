1e8078ec7eb807810e0b98bef11f81f9fc8543f9
V4n1sh795 <v4n1shnerush@gmail.com>
2026-06-06 19:54:37 +0500

    some fix

--- cash/obj/Debug/net10.0/cash.AssemblyInfo.cs
+++ cash/obj/Debug/net10.0/cash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29f369e9bc89b897565fccf9f881cf73ad77a3e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6bafecc5a1dcaf1e6fbc800771c004f223dd36b")]
 [assembly: System.Reflection.AssemblyProductAttribute("cash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
