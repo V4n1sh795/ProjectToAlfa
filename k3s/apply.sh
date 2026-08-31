39eebdb2e1096480f6eb4eccc9acbeb24a672edf
V4n1sh795 <v4n1shnerush@gmail.com>
2026-08-31 17:32:51 +0500

    fix apps problem

--- k3s/apply.sh
+++ k3s/apply.sh
@@ -10,7 +10,7 @@ k3s kubectl apply -f k3s/postgres/statefulset.yaml
 k3s kubectl apply -f k3s/postgres/service.yaml
 
 # 4. Ждём, пока PostgreSQL поднимется
-k3s kubectl -n myapp wait --for=condition=ready pod -l app=postgres --timeout=120s
+k3s kubectl -n alfa wait --for=condition=ready pod -l app=postgres --timeout=120s
 
 # 5. Backend
 k3s kubectl apply -f k3s/backend/deployment.yaml
